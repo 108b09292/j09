c4f328032a8ca32bdba495c0d06eecc9bfda0313
108b09292 <112999780+108b09292@users.noreply.github.com>
2022-10-26 09:45:37 +0800

    Add files via upload

---
+++ Class07.java
@@ -0,0 +1,17 @@
+package chap09;
+
+class A07{
+    public static void power(int x, int n){
+
+        System.out.println(Math.pow(x, n));
+
+    }
+}
+
+public class Class07 {
+    public static void main(String[] args) {
+
+        A07.power(2, 5);
+        A07.power(3, 2);
+    }
+}
